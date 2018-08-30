82a4c916e80959b20cdfbde1ad07e7e4435f5472
xuxiaoxu79 <wxyihui@gmail.com>
2018-08-30 10:43:46 +0800

    a

---
+++ ex2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# 注释和#号
+
+# A comment, this is so you can read your program later. 一段注释可以让你以后更好的阅读你的程序。
+# Anything after the # is ignored by python. ＃之后的任何内容都被python忽略。
+
+print("I could have code like this.")  # 这是放在代码行后面的注释
+
+# You can also use a comment to "disable" or comment out code: 您还可以使用注释来“禁用”或注释掉代码：
+# print("This won't run.")
+
+print("This will run.")
