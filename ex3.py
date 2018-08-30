4d9f860e5cce0e9e8ea981f4421e774b2610fef3
xuxiaoxu79 <wxyihui@gmail.com>
2018-08-30 11:30:36 +0800

    ex3

---
+++ ex3.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# 数字和数学计算
+
+print("I will now count my chickens(我现在开始统计我的鸡了):")
+# print使用逗号将多段内容连接到一起，不同的数据类型python会自动进行转换
+print("Hens母鸡", 25 + 30 / 6)
+print("Roosters公鸡", 100 - 25 * 3 % 4)  # %：取余数
+
+print("Now I will count the eggs(现在我开始统计鸡蛋):")
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Is it true that 3 + 2 < 5 - 7?")
+print(3 + 2 < 5 - 7)
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+print("Oh, that's why it's False.")
+
+print("How about some more(更多的计算).")
+print("Is it greater(它更大吗？)?", 5 >= -2)
+print("Is it less or equal(它是否小于或相等)?", 5 <= -2)
