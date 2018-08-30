4d5c5f10f5d0326a23b78a1d9c9b4e125899f3e0
xuxiaoxu79 <wxyihui@gmail.com>
2018-08-30 10:27:07 +0800

    first commit

---
+++ ex1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+# 第一个程序
+
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print("Yay! Printing.")
+print("I'd much rather you 'not'.")  # 在字符串中打印单引号
+print('I "said" do not touch this.')  # 在字符串中打印双引号
+
