5c5bab80f52135ed4d2ebd40e372fb5f73ff50d5
xuxiaoxu79 <wxyihui@gmail.com>
2018-09-02 22:20:49 +0800

    ex7

---
+++ ex7.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# 更多打印
+
+print("玛丽有只小羊羔。")
+print("它的羊毛是白色的像 {} 一样。".format('雪'))
+print("." * 10)  # 字符串的乘法运算相当于把指定的字符串复制若干次
+
+end1 = 'C'
+end2 = 'h'
+end3 = 'e'
+end4 = 'e'
+end5 = 's'
+end6 = 'e'
+end7 = 'B'
+end8 = 'u'
+end9 = 'r'
+end10 = 'g'
+end11 = 'e'
+end12 = 'r'
+
+# print 函数的 end 参数指定打印后以什么结尾，默认是回车
+print(end1 + end2 + end3 + end4 + end5 + end6, end='  ')
+print(end7 + end8 + end9 + end10 + end11 + end12)
