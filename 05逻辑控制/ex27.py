920ace490816f584307964226753de2a1f11bbce
xuxiaoxu79 <wxyihui@gmail.com>
2018-09-10 19:18:48 +0800

    ex28

--- 05逻辑控制/ex27.py
+++ 05逻辑控制/ex27.py
@@ -14,5 +14,38 @@
     True  真
     False  假
 
-
+真值表：
+        not                 真假
+    not False               True
+    not True                False
+        or
+    True or False           True
+    True or True            True
+    False or True           True
+    False or False          False
+        and
+    True and False          False
+    True and True           True
+    False and True          False
+    False and False         False
+        not or
+    not (True or False)     False
+    not (True or True)      False
+    not (False or True)     False
+    not (False or False)    True
+        not and
+    not (True and False)     True
+    not (True and True)      False
+    not (False and True)     True
+    not (False and False)    True
+        !=
+    1 != 0                  True
+    1 != 1                  False
+    0 != 1                  True
+    0 != 0                  False
+        ==
+    1 == 0                  False
+    1 == 1                  True
+    0 == 1                  False
+    0 == 0                  True
 '''
